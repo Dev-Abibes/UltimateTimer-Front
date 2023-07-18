232aef7bd4597d80c2f43655099f4e5e7c13a67d
AbDev <99358887+Dev-Abibes@users.noreply.github.com>
2023-07-18 15:46:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ setInterval(function () {
       timer.innerHTML = data.time;
     });
 }, 1000);
+
+function calculerTVA(montantHT, tauxTVA) {
+  if (typeof montantHT !== 'number' || typeof tauxTVA !== 'number' || tauxTVA < 0) {
+    throw new Error("Les montants doivent être des nombres positifs.");
+  }
+
+  const montantTVA = montantHT * (tauxTVA / 100);
+  const montantTTC = montantHT + montantTVA;
+
+  return {
+    montantHT: montantHT,
+    montantTVA: montantTVA,
+    montantTTC: montantTTC
+  };
+}
